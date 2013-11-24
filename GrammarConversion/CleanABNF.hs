49891f25a8a2a52775e1e92dc9670f3311295232
christina unger <f060047@gmail.com>
2013-11-24 10:27:33 +0100

    cleaned up grammar conversion

--- GrammarConversion/CleanABNF.hs
+++ GrammarConversion/CleanABNF.hs
@@ -4,11 +4,13 @@ import AbsABNF
 
 import Data.Map.Lazy as Hash hiding (map,filter,foldl)
 
+import Debug.Trace
+
 
 {- Cleaning ABNF grammar -}
 
 cleanUp :: Grammar -> Grammar
-cleanUp (Grammar defs) = Grammar $ (contractAndFilter . blowUp []) defs
+cleanUp (Grammar defs) = Grammar $ blowUp [] defs -- (contractAndFilter . blowUp []) defs
 
 
 -- split rules containing options and groups into several rules
@@ -20,28 +22,34 @@ blowUp done (def:defs)              = blowUp (done ++ [def]) defs
 
 expandFrag :: Fragment -> [Fragment]
 expandFrag (Fragment [])     = []
-expandFrag (Fragment (i:is)) = map (\ x -> Fragment x) (filter (/= []) (expandItems [[]] i is))
+expandFrag (Fragment (i:is)) = map (\ x -> Fragment x) (filter (/= []) (expandItems [] i is))
 
 expandItems :: [[Item]] -> Item -> [Item] -> [[Item]]
 expandItems done item []     = add done item
 expandItems done item (i:is) = expandItems (add done item) i is
 
 add :: [[Item]] -> Item -> [[Item]]
-add done  t@(Terminal    _)  = map (++[t])  done
-add done nt@(NonTerminal _)  = map (++[nt]) done
-add done    (Option choices) = done ++ addOptions choices done
-add done    (Group  choices) = addOptions choices done
+add []  t@(Terminal    _)  = [[t]] 
+add is  t@(Terminal    _)  = map (++[t])  is
+add [] nt@(NonTerminal _)  = [[nt]]
+add is nt@(NonTerminal _)  = map (++[nt]) is
+add is    (Option choices) = is ++ addOptions choices is
+add is    (Group  choices) = addOptions choices is
 
 addOptions :: [Choice] -> [[Item]] -> [[Item]]
-addOptions []     done = done
+addOptions [] done = done
 addOptions ((Choice     []):cs) done = addOptions cs done
-addOptions ((Choice (i:is)):cs) done = addOptions cs (done ++ (expandItems [[]] i is))
+addOptions ((Choice (i:is)):cs) done = addOptions cs (done ++ (expandItems [] i is))
 
+addItems :: [[Item]] -> [[Item]] -> [[Item]] 
+addItems done [] = done 
+addItems done ((i:is):items) = addItems (expandItems done i is) items
 
+{-
 -- contract and filter
 
 contractAndFilter :: [Def] -> [Def]
-contractAndFilter defs = buildRules $ contract (buildHash empty defs) defs -- buildRules $ removeNonsense $ contract ...
+contractAndFilter defs = buildRules $ removeNonsense $ contract (buildHash empty defs) defs
 
 buildHash :: Hash.Map Var [Fragment] -> [Def] -> Hash.Map Var [Fragment]
 buildHash hash [] = hash
@@ -49,7 +57,7 @@ buildHash hash ((Header):defs) = buildHash hash defs
 buildHash hash ((Root _):defs) = buildHash hash defs
 buildHash hash ((Rule lhs frags):defs) = buildHash (updateHash hash lhs frags) defs
        where updateHash hash lhs frags = if member (var lhs) hash 
-	                                        then Hash.adjust (++frags) (var lhs) hash
+	                                    then Hash.adjust (++frags) (var lhs) hash
                                             else Hash.insert (var lhs) frags hash
 
 contract :: Hash.Map Var [Fragment] -> [Def] -> Hash.Map Var [Fragment]
@@ -65,7 +73,7 @@ removeNonsense hash = foldl blah hash (keys hash)
      where blah h k = case (filterFrags h [] (h ! k)) of 
                             [] -> Hash.delete k h
                             fs -> Hash.insert k fs h
--- delete all keys that have a right-hand side 
+-- TODO delete all keys never occur on any RHS
 
 filterFrags :: Hash.Map Var [Fragment] -> [Fragment] -> [Fragment] -> [Fragment]
 filterFrags _    keep [] = keep
@@ -84,3 +92,5 @@ var (Public   v) = v
 
 buildRules :: Hash.Map Var [Fragment] -> [Def] 
 buildRules hash = map (\ k -> Rule (Internal k) (hash ! k)) (keys hash) 
+
+-}
